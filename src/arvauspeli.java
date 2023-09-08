d1872ad25798860b069987676b35139c6f6cfadf
IiroPeltonen <iiro23004@student.hamk.fi>
2023-09-08 18:46:11 +0300

    Start

--- src/arvauspeli.java
+++ src/arvauspeli.java
@@ -1,9 +1,18 @@
-
+import java.util.Scanner;
 public class arvauspeli {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner in = new Scanner(System.in);
+		
+		String name = "Teppo";
+		String guess;
+		int attemps = 0;
+		boolean correctGuess = false;
+		
+ 
+		
+		
+		
 	}
 
 }
